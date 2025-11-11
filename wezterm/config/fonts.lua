b0c5e948b18244142ab8891cfbdeef1bb5742cab
ronload <91997734+ronload@users.noreply.github.com>
2025-11-12 00:17:53 +0800

    feat(wezterm): change font family to menlo

--- wezterm/config/fonts.lua
+++ wezterm/config/fonts.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local M = {}
 
 function M.apply(config)
-	config.font = wezterm.font("Geist Mono", { weight = "Medium" })
+	config.font = wezterm.font("Menlo", { weight = "Medium" })
 	config.font_size = 16.0
 	config.line_height = 1.6
 
