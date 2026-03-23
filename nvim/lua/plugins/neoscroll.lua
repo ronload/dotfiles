0a21149b99c956ffe7c9499fd4f98b5ed137342e
ronload <91997734+ronload@users.noreply.github.com>
2026-03-23 18:11:40 +0800

    fix(nvim): add angle brackets which is missing

--- nvim/lua/plugins/neoscroll.lua
+++ nvim/lua/plugins/neoscroll.lua
@@ -2,7 +2,7 @@ return {
   "karb94/neoscroll.nvim",
   event = "VeryLazy",
   opts = {
-    mappings = { "C-u", "<C-d>", "<C-b>", "<C-f>", "zt", "zz", "zb" },
+    mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-f>", "zt", "zz", "zb" },
     hide_cursor = true,
     stop_eof = true,
     respect_scrolloff = false,
