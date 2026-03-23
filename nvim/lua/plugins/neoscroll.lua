8e785229cfed09a4390d225fe4b9371ceacb308f
ronload <91997734+ronload@users.noreply.github.com>
2026-03-23 18:08:04 +0800

    fix(nvim): resolve typo

--- nvim/lua/plugins/neoscroll.lua
+++ nvim/lua/plugins/neoscroll.lua
@@ -1,7 +1,7 @@
 return {
   "karb94/neoscroll.nvim",
   event = "VeryLazy",
-  opt = {
+  opts = {
     mappings = { "C-u", "<C-d>", "<C-b>", "<C-f>", "zt", "zz", "zb" },
     hide_cursor = true,
     stop_eof = true,
