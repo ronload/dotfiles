6ff907aaa3e892fd1b732b50522b437b3abd607a
ronload <91997734+ronload@users.noreply.github.com>
2026-04-06 01:04:14 +0800

    fix(nvim): migrate treesitter-textobjects to main branch API

--- nvim/lua/plugins/treesitter-textobject.lua
+++ nvim/lua/plugins/treesitter-textobject.lua
@@ -1,20 +1,23 @@
 return {
   "nvim-treesitter/nvim-treesitter-textobjects",
+  branch = "main",
   dependencies = { "nvim-treesitter/nvim-treesitter" },
   config = function()
-    require("nvim-treesitter.configs").setup({
-      textobjects = {
-        select = {
-          enable = true,
-          lookahead = true,
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-          },
-        },
+    require("nvim-treesitter-textobjects").setup({
+      select = {
+        lookahead = true,
       },
     })
+
+    local select_textobject = function(query)
+      return function()
+        require("nvim-treesitter-textobjects.select").select_textobject(query, "textobjects")
+      end
+    end
+
+    vim.keymap.set({ "x", "o" }, "af", select_textobject("@function.outer"), { desc = "Select outer function" })
+    vim.keymap.set({ "x", "o" }, "if", select_textobject("@function.inner"), { desc = "Select inner function" })
+    vim.keymap.set({ "x", "o" }, "ac", select_textobject("@class.outer"), { desc = "Select outer class" })
+    vim.keymap.set({ "x", "o" }, "ic", select_textobject("@class.inner"), { desc = "Select inner class" })
   end,
 }
