609e910589153a69ad183b123045f7ade789bb60
ronload <91997734+ronload@users.noreply.github.com>
2026-04-05 14:44:06 +0800

    refactor: unify colorscheme config

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,35 @@ return {
     opts = {
       style = "night",
       transparent = true,
+      on_highlights = function(hl, c)
+        -- Telescope
+        hl.TelescopeNormal = { bg = c.bg_dark, fg = c.fg }
+        hl.TelescopeBorder = { bg = c.bg_dark, fg = c.comment }
+        hl.TelescopePromptNormal = { bg = c.bg_dark, fg = c.fg }
+        hl.TelescopePromptBorder = { bg = c.bg_dark, fg = c.comment }
+        hl.TelescopePromptTitle = { bg = c.blue, fg = c.bg_dark }
+        hl.TelescopePreviewTitle = { bg = c.green, fg = c.bg_dark }
+        hl.TelescopeResultsTitle = { bg = c.magenta, fg = c.bg_dark }
+        hl.TelescopeSelection = { bg = c.bg_highlight, fg = c.fg }
+        hl.TelescopeSelectionCaret = { bg = c.bg_highlight, fg = c.magenta }
+        hl.TelescopeMatching = { fg = c.cyan, bold = true }
+
+        -- NvimTree
+        hl.NvimTreeNormal = { bg = c.bg, fg = c.fg }
+        hl.NvimTreeNormalNC = { bg = c.bg, fg = c.fg }
+        hl.NvimTreeWinSeparator = { bg = c.bg, fg = c.comment }
+        hl.NvimTreeFolderName = { fg = c.blue }
+        hl.NvimTreeOpenedFolderName = { fg = c.blue, bold = true }
+        hl.NvimTreeFolderIcon = { fg = c.blue }
+        hl.NvimTreeRootFolder = { fg = c.magenta, bold = true }
+        hl.NvimTreeGitDirty = { fg = c.orange }
+        hl.NvimTreeGitNew = { fg = c.green }
+        hl.NvimTreeGitDeleted = { fg = c.red }
+        hl.NvimTreeSpecialFile = { fg = c.yellow }
+        hl.NvimTreeIndentMarker = { fg = c.comment }
+        hl.NvimTreeCursorLine = { bg = c.bg_highlight }
+        hl.NvimTreeGitIgnored = { fg = c.dark3 }
+      end,
     },
     config = function(_, opts)
       require("tokyonight").setup(opts)
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -20,35 +20,6 @@ return {
       { "<leader><leader>", "<cmd>Telescope find_files<cr>", desc = "Find files" },
     },
     config = function()
-      -- tokyonight-night palette
-      local c = {
-        bg = "#1a1b26",
-        bg_dark = "#16161e",
-        bg_highlight = "#292e42",
-        fg = "#c0caf5",
-        fg_dark = "#a9b1d6",
-        comment = "#565f89",
-        blue = "#7aa2f7",
-        cyan = "#7dcfff",
-        green = "#9ece6a",
-        magenta = "#bb9af7",
-        orange = "#ff9e64",
-        red = "#f7768e",
-        yellow = "#e0af68",
-      }
-
-      local hl = vim.api.nvim_set_hl
-      hl(0, "TelescopeNormal", { bg = c.bg_dark, fg = c.fg })
-      hl(0, "TelescopeBorder", { bg = c.bg_dark, fg = c.comment })
-      hl(0, "TelescopePromptNormal", { bg = c.bg_dark, fg = c.fg })
-      hl(0, "TelescopePromptBorder", { bg = c.bg_dark, fg = c.comment })
-      hl(0, "TelescopePromptTitle", { bg = c.blue, fg = c.bg_dark })
-      hl(0, "TelescopePreviewTitle", { bg = c.green, fg = c.bg_dark })
-      hl(0, "TelescopeResultsTitle", { bg = c.magenta, fg = c.bg_dark })
-      hl(0, "TelescopeSelection", { bg = c.bg_highlight, fg = c.fg })
-      hl(0, "TelescopeSelectionCaret", { bg = c.bg_highlight, fg = c.magenta })
-      hl(0, "TelescopeMatching", { fg = c.cyan, bold = true })
-
       local telescope = require("telescope")
       telescope.setup({
         defaults = {
