8d57abb2d4a072b1a34577398edf08cf9c3e9bf8
ronload <91997734+ronload@users.noreply.github.com>
2026-04-06 01:04:07 +0800

    fix(nvim): migrate treesitter config to new API

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,38 +3,27 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    opts = {
-      fold = { enable = true },
-      ensure_installed = {
-        "go",
-        "gomod",
-        "gosum",
-        "typescript",
-        "tsx",
-        "javascript",
-        "python",
-        "rust",
-        "lua",
-        "json",
-        "yaml",
-        "html",
-        "css",
-        "markdown",
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
+    config = function()
+      require("nvim-treesitter").install({
+        "go", "gomod", "gosum",
+        "typescript", "tsx", "javascript",
+        "python", "rust", "lua",
+        "json", "yaml", "html", "css", "markdown",
+      })
+
+      local function enable_treesitter(buf)
+        if pcall(vim.treesitter.start, buf) then
+          vim.bo[buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end
+      end
+
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(ev)
+          enable_treesitter(ev.buf)
+        end,
+      })
+
+      enable_treesitter(vim.api.nvim_get_current_buf())
     end,
   },
 }
