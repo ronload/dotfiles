fd8b858ea195468b8068a30d43413811d755e918
ronload <91997734+ronload@users.noreply.github.com>
2026-05-07 13:05:02 +0800

    feat(zsh): add eza aliases for ls, tree and fuzzy file preview

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,6 +18,16 @@ alias :q="exit"
 alias countcode="tokei . -e node_modules -e .venv -e .next -e dist -e build -e out"
 alias c=claude
 
+# eza
+alias ls='eza --icons=auto --group-directories-first'
+alias ll='eza -l --icons=auto --git --group-directories-first --time-style=relative'
+alias la='eza -la --icons=auto --git --group-directories-first'
+alias lr='eza -l --icons=auto --git --sort=modified --time-style=relative'
+alias lt='eza --tree --level=2 --icons=auto --git-ignore'
+alias lt3='eza --tree --level=3 --icons=auto --git-ignore'
+alias lgit='eza -la --icons=auto --git --git-ignore --group-directories-first'
+alias lf='eza --tree --level=3 --icons=auto --git-ignore | fzf --preview "bat --color=always {}"'
+
 # navigation
 alias ..="cd .."
 alias dotfiles="cd ~/dotfiles/"
