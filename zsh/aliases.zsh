40bc34923612940d292aedbee7b8ceaeacf3122b
ronload <91997734+ronload@users.noreply.github.com>
2026-05-11 16:56:51 +0800

    refactor(zsh): reorganize abbreviations and aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,11 +6,12 @@ glb() {
 
 # tools
 run() { chmod +x "$1" && ./"$1"; }
-alias c=claude
 alias cat="bat"
-alias jq="jaq"
+alias fastfetch="macchina"
 alias http="xh"
 alias https="xhs"
+alias jq="jaq"
+alias top="btm"
 alias vi='nvim'
 alias vim='nvim'
 
@@ -19,8 +20,5 @@ alias ls='eza --icons=auto --group-directories-first'
 alias ll='eza -l --icons=auto --git --group-directories-first --time-style=relative'
 alias la='eza -la --icons=auto --git --group-directories-first'
 alias lr='eza -l --icons=auto --git --sort=modified --time-style=relative'
-alias lt='eza --tree --level=2 --icons=auto --git-ignore'
-alias lt3='eza --tree --level=3 --icons=auto --git-ignore'
-alias lgit='eza -la --icons=auto --git --git-ignore --group-directories-first'
 alias lf='eza --tree --level=3 --icons=auto --git-ignore | fzf --preview "bat --color=always {}"'
-
+alias lgit='eza -la --icons=auto --git --git-ignore --group-directories-first'
