a114f96d7701f0ea6bc7c86c06a2750b98bd7304
ronload <91997734+ronload@users.noreply.github.com>
2026-06-27 07:01:35 +0800

    feat(nvim): replace yazi with oil for file navigation
    Switch the file explorer to oil.nvim (editable buffer-style navigation) with mini.icons; drop yazi.nvim and its netrw override.

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,11 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+  keys = {
+    { "-", "<CMD>Oil<CR>", desc = "Open parent directory" },
+  },
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {},
+  lazy = false,
+}
